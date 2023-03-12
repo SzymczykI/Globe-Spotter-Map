129d21974cf4181c1de3776259a40269d040d9a6
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2023-03-12 17:19:21 +0100

    markers and texzt layers

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import Map, { NavigationControl } from "react-map-gl";
 import DeckOverlay from "./DeckOverlay";
 import { fetchData } from "../helpers/dataFetcher";
+import { IconLayer, TextLayer } from "@deck.gl/layers";
+import pin from "../assets/pin.png";
 
 const InteractiveMap = () => {
   const token = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
@@ -19,6 +21,31 @@ const InteractiveMap = () => {
       .then((res) => setCountriesData(res))
       .catch((err) => console.log(err));
   }, []);
+
+  const countriesLayer = new IconLayer({
+    id: "countries",
+    data: countriesData,
+    getPosition: (d: { latlng: number[] }) => [d.latlng[1], d.latlng[0]],
+    getIcon: () => ({
+      url: pin,
+      width: 128,
+      height: 128,
+    }),
+    sizeMinPixels: 20,
+  });
+
+  const capitalsLayer = new TextLayer({
+    id: "capitals",
+    data: countriesData,
+    getPosition: (d: { capitalInfo: { latlng: number[] } }) =>
+      d.capitalInfo.latlng
+        ? [d.capitalInfo.latlng[1], d.capitalInfo.latlng[0]]
+        : [0, 0],
+    getText: (d: { capital: string[] }) => (d.capital ? d.capital[0] : ""),
+    getColor: [78, 84, 82],
+    getSize: 10,
+    getAlignmentBaseline: "center"
+  });
 
   return (
     <Map
