c813a17ada3f082e3ad1f95d319cc7762b83b284
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2023-03-12 16:41:54 +0100

    navigation control added

--- src/components/InteractiveMap.tsx
+++ src/components/InteractiveMap.tsx
@@ -1,5 +1,5 @@
-import {useState} from "react";
-import Map, { NavigationControl } from 'react-map-gl'
+import { useState } from "react";
+import Map, { NavigationControl } from "react-map-gl";
 
 const InteractiveMap = () => {
   const token = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
@@ -7,22 +7,22 @@ const InteractiveMap = () => {
   const [mapViewState, setMapViewState] = useState({
     longitude: 0,
     latitude: 0,
-    zoom: 2
-  })
+    zoom: 2,
+  });
 
   return (
-  <Map 
-  mapboxAccessToken={token}
-  mapStyle="mapbox://styles/szymczyki/clf4gnmia005x01mrdaz1cdx4"
-  {...mapViewState}
-  onMove={(next) => setMapViewState(next.viewState)}
-  style={{width: '100vh', height: '100vh'}}
-  >
-<NavigationControl />
-
-
-  </Map>
-  )
+    <Map
+      mapboxAccessToken={token}
+      mapStyle="mapbox://styles/szymczyki/clf4gnmia005x01mrdaz1cdx4"
+      {...mapViewState}
+      onMove={(next) => setMapViewState(next.viewState)}
+      style={{ width: "100vw", height: "100vh" }}
+      cursor="auto"
+      renderWorldCopies={false}
+    >
+      <NavigationControl />
+    </Map>
+  );
 };
 
 export default InteractiveMap;
