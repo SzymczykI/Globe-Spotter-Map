99e75771689909c2508b200bbfd799b0ed49af2e
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2023-03-12 23:02:47 +0100

    some styling

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const InfoBox = () => {
   return (
-    <section className="absolute bottom-1 right-1 bg-white/80  text-center w-80 z-10 p-6 rounded-lg shadow-md" >
+    <section className="absolute bottom-1 right-1 bg-white/80 text-center text-xs md:text-base w-56 md:w-80 z-10 p-3 md:p-6 rounded-lg shadow-md" >
       <h1 className="font-bold">Interactive Countries Map</h1>
       <p>
         The markers are designed to display information about the country. On
