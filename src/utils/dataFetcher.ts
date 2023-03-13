6cf8d8a05c457148ebe88c7f12b22e1d2986efa9
Izabela Szymczyk <izabela.szymczyk@vp.pl>
2023-03-13 23:10:45 +0100

    fetchData refactored

--- src/utils/dataFetcher.ts
+++ src/utils/dataFetcher.ts
@@ -1,7 +1,9 @@
 export const fetchData = async () => {
-    const data = fetch('https://restcountries.com/v3.1/all')
-    .then(res => res.json())
-    .catch(err => console.log(err));
-
-    return data
-}
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    const data = response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
